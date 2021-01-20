217f430ec8bd99df5cdd6128a3c314e0c164bf3f
Michael Fuget <michaelfuget@gmail.com>
2021-01-20 03:34:27 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,4 @@
 class Student < ActiveRecord::Base
-  after_initialize :set_defaults, unless: :persisted?
-  def set_defaults
-    self.active  ||= false
-  end
 
   def to_s
     self.first_name + " " + self.last_name
